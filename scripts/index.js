1da839c83bda02fcdaace5b3bd6e1dee2980f8e1
KODKOD177 <pavel2012155@gmail.com>
2023-11-10 12:24:17 +0200

    submit function

--- scripts/index.js
+++ scripts/index.js
@@ -27,18 +27,31 @@ let initialCards = [
 
 const editButton = document.querySelector(".profile__edit-button");
 const modal = document.querySelector(".modal");
-const closeEditButton = document.querySelector(".modal__closeButton");
+const closeEditButton = modal.querySelector(".modal__closeButton");
+const submitButton = modal.querySelector(".modal__form-button");
+
+const profileName = document.querySelector(".profile__name"); //the profile name from the page
+const nameInput = modal.querySelector("#modal_form-name"); //the profile name input in modal
+
+const profileJob = document.querySelector(".profile__descripton"); //the profile job from the page
+const jobInput = modal.querySelector("#modal_form-job"); //the profile job input form modal
 
 editButton.addEventListener("click", function () {
   modal.classList.add("modal_opened");
-  let nameForm = document.querySelector(".profile__name");
-  let modalFormName = document.querySelector("#modal_form-name");
-  modalFormName.value = nameForm.textContent;
-  let jodForm = document.querySelector(".profile__descripton");
-  let modalFormJob = document.querySelector("#modal_form-job");
-  modalFormJob.value = jodForm.textContent;
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 });
 
 closeEditButton.addEventListener("click", function () {
   modal.classList.remove("modal_opened");
 });
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault(); //preventin the refresh of the page
+
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  modal.classList.remove("modal_opened");
+}
+
+modal.addEventListener("submit", handleProfileFormSubmit);
